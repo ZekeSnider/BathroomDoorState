fcf875a3ee4a0b30e9a1ff79938d4c55055b3ba8
Zeke Snider <zeke.snider@fujifilm.com>
2017-12-07 16:24:34 -0800

    Add client code

---
+++ client/index.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#https://github.com/mxgxw/MFRC522-python
+
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+import requests
+import json
+import time
+
+SLEEP_TIME = 500
+continue_reading = True
+
+class DoorState(enum):
+    available = 1
+    occupied = 2
+
+lastState = DoorState.available
+
+with open('config.json') as json_data_file:
+    configData = json.loads(json_data_file)
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
+    global continue_reading
+    continue_reading = False
+    GPIO.cleanup()
+
+def update_server_state(state):
+    payload = {'state': state.name, 'key': configData['key']}
+    requests.post(data['apiEndpoint'], data=json.dumps(payload))
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+while continue_reading:
+    # Scan for cards    
+    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    if status == MIFAREReader.MI_OK:
+        currentState = DoorState.available
+    else:
+        currentState = DoorState.occupied
+
+    if (lastState != currentState):
+        update_server_state(currentState)
+
+    lastState = currentState
+    time.sleep(SLEEP_TIME/1000) 
+        
